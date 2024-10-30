ec979fbd4af17de4520c397b112c3bf2c3ce4464
Emre-Guzel-2 <yunus.emre.guzel@mths.ca>
2024-10-30 15:23:26 +0000

    added buton a and changed the names

--- main.ts
+++ main.ts
@@ -1,8 +1,19 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: Mr. Coxall
- * Created on: Sep 2020
- * This program ...
+ * Created by: Emre Guzel
+ * Created on: Oct 30 2024
+ * This program tuns 2 mootrs and stops them when the distacne is smaler tahn 10 cm
 */
+// Setting the varibels 
+let distacne: number = 0
 
-basic.showString('Hello, World!')
+// Setting the screen
+basic.clearScreen()
+basic.showIcon(IconNames.Happy)
+
+// When it is clikes a it will going to mesuare the distacne and make the mootrs work 
+input.onButtonPressed(Button.A,function(){
+
+})
+
+
