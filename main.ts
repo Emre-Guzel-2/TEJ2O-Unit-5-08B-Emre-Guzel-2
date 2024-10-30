163c43486e4e5b8b6d27e61c37bb6a6e14b3fa3e
Emre-Guzel-2 <yunus.emre.guzel@mths.ca>
2024-10-30 16:03:42 +0000

    added sonar library and the motor library

--- main.ts
+++ main.ts
@@ -5,7 +5,7 @@
  * This program tuns 2 mootrs and stops them when the distacne is smaler tahn 10 cm
 */
 // Setting the varibels 
-let distacne: number = 0
+let distacneToObject: number = 0
 
 // Setting the screen
 basic.clearScreen()
@@ -13,7 +13,19 @@ basic.showIcon(IconNames.Happy)
 
 // When it is clikes a it will going to mesuare the distacne and make the mootrs work 
 input.onButtonPressed(Button.A,function(){
+basic.clearScreen()
 
+// Setting the sonar
+distacneToObject = sonar.ping(
+    DigitalPin.P1,
+    DigitalPin.P2,
+    PingUnit.Centimeters
+)
+    // Setting the motors 
+    robotbit.StepperTurn(robotbit.Steppers.M1, robotbit.Turns.T1B4)
+    robotbit.StepperTurn(robotbit.Steppers.M2, robotbit.Turns.T1B4)
+    
+    basic.showNumber(distacneToObject)
 })
 
 
