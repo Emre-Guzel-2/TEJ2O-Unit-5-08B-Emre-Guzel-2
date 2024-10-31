4daa1da9e0cd7b42677a04f7bb7c5350481ddfd3
Emre-Guzel-2 <yunus.emre.guzel@mths.ca>
2024-10-31 14:38:01 +0000

    inteadted

--- main.ts
+++ main.ts
@@ -11,35 +11,35 @@ basic.clearScreen()
 basic.showIcon(IconNames.Happy)
 
 // When it is clikes a it will going to mesuare the distacne and make the mootrs work 
-input.onButtonPressed(Button.A,function(){
-basic.clearScreen()
+input.onButtonPressed(Button.A, function () {
+    basic.clearScreen()
 
-while (1==1){
-    // Setting the motors 
-   distacneToObject = sonar.ping(
-    // Setting the sonar
-    DigitalPin.P1,
-    DigitalPin.P2,
-    PingUnit.Centimeters
-)
-      basic.showNumber(distacneToObject)
-    //If distacneToObject is biiger than 10 makes the motros go forward 
-    if (distacneToObject > 10){
-        robotbit.MotorRunDual(robotbit.Motors.M1A, 150, robotbit.Motors.M2A, 150)
-        robotbit.StpCarMove(10, 48)
-    }
-    else {
-//If distacneToObject is smaller than 10 it stops the mootrs
-        robotbit.MotorStopAll()
-        // Moves backward for  10 cm
-        robotbit.StpCarMove(-10, 48)
-        // Turning 90 degreese 
-        robotbit.StepperTurn(robotbit.Steppers.M1, robotbit.Turns.T1B4)
-        robotbit.StepperTurn(robotbit.Steppers.M2, robotbit.Turns.T1B4)
-        // Setting the speed of the motors and seting the motors
-        robotbit.MotorRunDual(robotbit.Motors.M1A, 150, robotbit.Motors.M2A, 150)
-        robotbit.StpCarMove(10, 48)
+    while (1 == 1) {
+        // Setting the motors 
+        distacneToObject = sonar.ping(
+            // Setting the sonar
+            DigitalPin.P1,
+            DigitalPin.P2,
+            PingUnit.Centimeters
+        )
+        basic.showNumber(distacneToObject)
+        //If distacneToObject is biiger than 10 makes the motros go forward 
+        if (distacneToObject > 10) {
+            robotbit.MotorRunDual(robotbit.Motors.M1A, 150, robotbit.Motors.M2A, 150)
+            robotbit.StpCarMove(10, 48)
+        }
+        else {
+            //If distacneToObject is smaller than 10 it stops the mootrs
+            robotbit.MotorStopAll()
+            // Moves backward for  10 cm
+            robotbit.StpCarMove(-10, 48)
+            // Turning 90 degreese 
+            robotbit.StepperTurn(robotbit.Steppers.M1, robotbit.Turns.T1B4)
+            robotbit.StepperTurn(robotbit.Steppers.M2, robotbit.Turns.T1B4)
+            // Setting the speed of the motors and seting the motors
+            robotbit.MotorRunDual(robotbit.Motors.M1A, 150, robotbit.Motors.M2A, 150)
+            robotbit.StpCarMove(10, 48)
 
+        }
     }
-}
 })
